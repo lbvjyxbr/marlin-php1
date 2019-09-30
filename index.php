66312182414878ab366715168c4af97fe62d482d
Your Name <you@example.com>
2019-10-01 01:34:24 +0300

    change

--- index.php
+++ index.php
@@ -13,56 +13,57 @@
     <!-- Styles -->
     <link href="css/app.css" rel="stylesheet">
 </head>
-
 <?php $comments = [
-     'user_image' => 'img/no-user.jpg',
+    ['user_image' => 'img/no-user.jpg',
      'user_name' => 'John Doe',
      'date_comment' => '2019-08-22',
-     'user_comment' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aspernatur, ullam doloremque deleniti, sequi obcaecati.'];
+     'user_comment' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe aspernatur, ullam doloremque deleniti, sequi obcaecati.']
+];
 ?>
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">
-                Project
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">
+                    Project
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-                </ul>
+                    </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.html">Login</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="register.html">Register</a>
-                    </li>
-                </ul>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="login.html">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="register.html">Register</a>
+                            </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 
-    <main class="py-4">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header"><h3>Комментарии</h3></div>
+        <main class="py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header"><h3>Комментарии</h3></div>
 
-                        <div class="card-body">
-                            <div class="alert alert-success" role="alert">
+                            <div class="card-body">
+                              <div class="alert alert-success" role="alert">
                                 Комментарий успешно добавлен
-                            </div>
-                            <?php foreach ($comments as $comment): ?>
+                              </div>
+
+                                <?php foreach ($comments as $comment): ?>
                                 <div class="media">
                                     <img src="<?php echo $comment['user_image']; ?>" class="mr-3" alt="..." width="64" height="64">
                                     <div class="media-body">
@@ -74,32 +75,32 @@
                                     </div>
                                 </div>
                             <?php endforeach; ?>
+                            </div>
                         </div>
                     </div>
-                </div>
+                
+                    <div class="col-md-12" style="margin-top: 20px;">
+                        <div class="card">
+                            <div class="card-header"><h3>Оставить комментарий</h3></div>
 
-                <div class="col-md-12" style="margin-top: 20px;">
-                    <div class="card">
-                        <div class="card-header"><h3>Оставить комментарий</h3></div>
-
-                        <div class="card-body">
-                            <form action="/store" method="post">
-                                <div class="form-group">
+                            <div class="card-body">
+                                <form action="/store" method="post">
+                                    <div class="form-group">
                                     <label for="exampleFormControlTextarea1">Имя</label>
                                     <input name="name" class="form-control" id="exampleFormControlTextarea1" />
-                                </div>
-                                <div class="form-group">
+                                  </div>
+                                  <div class="form-group">
                                     <label for="exampleFormControlTextarea1">Сообщение</label>
                                     <textarea name="text" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                                </div>
-                                <button type="submit" class="btn btn-success">Отправить</button>
-                            </form>
+                                  </div>
+                                  <button type="submit" class="btn btn-success">Отправить</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
-</div>
+        </main>
+    </div>
 </body>
 </html>
